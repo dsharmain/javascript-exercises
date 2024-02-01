8077ff32fe00cfca632671a4cda2c943414afda6
Dipin Sharma <dsharmain@gmail.com>
2024-02-01 17:00:18 -0500

    Complete 1st set

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitArray = str.split('');
+    let reverse = '';
+    for(let i =0; i < splitArray.length; i++) {
+        //console.log(splitArray[splitArray.length - i - 1]);
+        reverse += splitArray[splitArray.length - i - 1];
+    }
+    //console.log(splitArray);
+    return reverse;
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    console.log(array.length);
+    console.log(array);
+    console.log(theArgs.length);
+    for (let i=0; i<theArgs.length; i++) {
+        console.log(array.indexOf(theArgs[i]));
+        if (array.indexOf(theArgs[i]) === -1) {
+            continue;
+        }
+        array.splice(array.indexOf(theArgs[i]), 1);
+    }
+    console.log(array);
+    return array;
 };
 
 // Do not edit below this line
